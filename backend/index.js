9c6c0cc3b8ffc45d57aeb84e5f02759bb393564c
bobanboshevski <152434305+bobanboshevski@users.noreply.github.com>
2025-05-03 20:56:57 +0200

    Modification so the FE and BE can communicate

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3001; // process.env.PORT
+
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://privacy-policy-analyzer-xi.vercel.app'
+    ]
+}));
 
 app.get('/', (req, res) => {
     res.send('Backend is working :))!');
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,34 @@
 // import Image from "next/image";
+'use client';
+
+
+import {useEffect, useState} from "react";
 
 export default function Home() {
-  return (
-    <div className="grid min-h-screen items-center justify-items-center bg-black text-white">
-      <main className="flex flex-col gap-4 items-center">
-          <h1 className="text-4xl font-bold">Hello World</h1>
 
-      </main>
-    </div>
-  );
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(apiUrl || '');
+                const text = await response.text();
+                setMessage(text)
+            } catch (err) {
+                console.error("No communication with backend!");
+                setMessage("Error getting data from backend!");
+            }
+        };
+        fetchData();
+    },[]);
+
+    return (
+        <div className="grid min-h-screen items-center justify-items-center bg-black text-white">
+            <main className="flex flex-col gap-4 items-center">
+                <h1 className="text-4xl font-bold">Hello World</h1>
+                <p>{message}</p>
+            </main>
+        </div>
+    );
 }
