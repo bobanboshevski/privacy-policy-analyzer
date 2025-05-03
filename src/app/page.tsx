fc67be979bdefc24e595d7abf0426d85eef0bf79
bobanboshevski <152434305+bobanboshevski@users.noreply.github.com>
2025-05-03 13:07:27 +0200

    Initial NextJS setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid min-h-screen items-center justify-items-center bg-black text-white">
+      <main className="flex flex-col gap-4 items-center">
+          <h1 className="text-4xl font-bold">Hello World</h1>
+
+      </main>
+    </div>
+  );
+}
