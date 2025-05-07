30dd780cad82dd576a7f97fe7072ba5d16379bcc
bobanboshevski <152434305+bobanboshevski@users.noreply.github.com>
2025-05-08 00:25:43 +0200

    deploy fix 2

--- backend/index.js
+++ backend/index.js
@@ -6,5 +6,5 @@ const config = require('./src/config/environment');
 const PORT = config.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}!`);
 });
